e95abd5f01b0ecf044af31e12f03ee43df4421d8
Song Jae Hyeon <0nocsace@gmail.com>
2021-08-06 22:17:55 +0900

    Vue add product

---
+++ src/components/ProductPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="nav-bar"></div>
+    <div class="cart">
+      <p>Cart({{ cart.length }})</p>
+    </div>
+    <Product @add-to-cart="addToCart"></Product>
+  </div>
+</template>
+<script>
+import Product from "./Product.vue";
+export default {
+  name: "ProductPage",
+  components: {
+    Product,
+  },
+  data() {
+    return { cart: [] };
+  },
+  methods: {
+    addToCart(variantId) {
+      this.cart.push(variantId);
+    },
+  },
+};
+</script>
